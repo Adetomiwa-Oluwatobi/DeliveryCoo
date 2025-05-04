fe9d3be9875b364f67efa8cf3fbed9f367d1debc
Adetomiwa-Oluwatobi <adetomiwaoluwatobi45@gmail.com>
2025-05-04 12:33:50 +0100

    new db added to .env

--- delivery_service/settings.py
+++ delivery_service/settings.py
@@ -78,14 +78,21 @@ WSGI_APPLICATION = 'delivery_service.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import dj_database_url
+from decouple import config
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = config('DATABASE_URL', default=None)
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
