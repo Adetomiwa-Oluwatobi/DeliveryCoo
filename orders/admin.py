bec2408a293e6424d02d9d4ed8d4596f0e992de3
Adetomiwa-Oluwatobi <adetomiwaoluwatobi45@gmail.com>
2025-05-05 18:50:52 +0100

    update on admin dashboad

--- orders/admin.py
+++ orders/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Company, Client, Order, DeliveryPersonnel, OrderTracking
+from .models import Company, Client, Order, DeliveryPersonnel, OrderTracking,Category
 
 admin.site.register(Company)
 admin.site.register(Client)
 admin.site.register(Order)
 admin.site.register(DeliveryPersonnel)
 admin.site.register(OrderTracking)
+admin.site.register(Category)
