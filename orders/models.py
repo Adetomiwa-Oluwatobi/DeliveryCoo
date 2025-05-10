10409cc3d0d143333b18ad7203dd8634cfb002c0
Adetomiwa-Oluwatobi <adetomiwaoluwatobi45@gmail.com>
2025-05-10 10:53:36 +0100

    tracking ui

--- orders/models.py
+++ orders/models.py
@@ -81,7 +81,7 @@ class Company(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='company_profile')
     name = models.CharField(max_length=100)
     address = models.TextField()
-    logo = models.ImageField(upload_to='company_logos/', null=True, blank=True)
+    logo = CloudinaryField('image',folder='company_logos/', null=True, blank=True)
     
     def __str__(self):
         return self.name
