b65ba4428fd4b3b20f04624f063de1be42e52506
Adetomiwa-Oluwatobi <adetomiwaoluwatobi45@gmail.com>
2025-05-10 09:37:41 +0100

    visitors role

--- orders/models.py
+++ orders/models.py
@@ -161,7 +161,7 @@ class DeliveryLog(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    image = CloudinaryField(upload_to='category_images/', null=True, blank=True)
+    image = CloudinaryField('image', folder='category_images/', null=True, blank=True)
     
     def __str__(self):
         return self.name
@@ -177,7 +177,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.SET_NULL, null=True)
     available = models.BooleanField(default=True)
-    image = CloudinaryField(upload_to='product_images/', null=True, blank=True)
+    image = CloudinaryField('image', folder='product_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
