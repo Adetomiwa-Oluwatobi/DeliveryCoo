cea5fd44fed5d54187f13d4b958ce6c30799df9f
Adetomiwa-Oluwatobi <adetomiwaoluwatobi45@gmail.com>
2025-06-04 09:52:54 +0100

    overlay chat bot

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'chatbot'
 
 urlpatterns = [
-    path('', views.chatbot_page, name='chatbot_page'),
+    path('chat/', views.chatbot_page, name='chatbot_page'),
     path('api/', views.chatbot_api, name='chatbot_api'),
 ]
